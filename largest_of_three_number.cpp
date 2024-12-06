2e26a62c382f15faf455930ff81553f37324ae6e
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2024-12-06 14:52:11 +0530

    largest_of_three_number.cpp

---
+++ largest_of_three_number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout<<"enter first number";
+    cin>>a;
+    cout<<"enter second number";
+    cin>>b;
+    cout<<"enter third number";
+    cin>>c;
+    if((a>b)&&(a>c)){
+        cout<<("the largest number is");
+        cout<<a;
+    }
+    else if((b>a)&&(b>c)){
+        cout<<"the largest number is";
+        cout<<b;
+    }
+    else if((c>a)&&(c>b)){
+        cout<<"the largest number is";
+        cout<<c;
+    }
+    else{
+        cout<<"all the numbers are equal";
+    }
+    return 0;
+}

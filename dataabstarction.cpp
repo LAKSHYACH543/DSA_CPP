c80eb2cef120e78804b9ce3acc22f2b7adc49d76
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2024-12-09 14:32:35 +0530

    dataabstarction.cpp

---
+++ dataabstarction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int a=3;
+    int b=4;
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"after swapping is"<<endl;
+    swap(a,b);
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"the value of b is "<<b<<endl;
+    int result=pow(a,b);
+    cout<<"the result is "<<result<<endl;
+    std::cout << "Try programiz.pro";
+
+    return 0;
+}

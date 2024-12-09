7a5fbbda8d8cf1295f43c3edc7fa6b06b9a11fec
LAKSHYA CHOUDHARY <151423785+LAKSHYACH543@users.noreply.github.com>
2024-12-09 14:29:20 +0530

    dataabstraction.cpp

---
+++ dataabstraction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main() {
+    int a=3;
+    int b=4;
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"after swapping is"<<endl;
+    swap(a,b);
+    cout<<"the value of a is"<<a<<endl;
+    cout<<"the value of b is "<<b<<endl;
+    std::cout << "Try programiz.pro";
+
+    return 0;
+}
